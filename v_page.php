09a455a5bf9ae19c4fee79fc1894e5e248ada3d7
Pavel <pavel_zobov01@inbox.ru>
2021-05-19 22:35:59 +0300

    yes

--- v_page.php
+++ v_page.php
@@ -1,5 +1,7 @@
 <?php
 if(isset($_GET['go'])) header('Location: b_page.php');
+session_start();
+$nm = $_SESSION['nams'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,25 +21,44 @@ if ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
 }
 $_GB = new GuestBook();
 $i = 1;
+$k = 1;
 while ($pid >= $i){
     $entries = $_GB->get($i);
-    ?>
-    <div id="gb-entries">
+    $dels = 'delete' . $k;?>
+    <div id='gb-entries'>
         <?php if (count($entries)>0) { foreach ($entries as $e) { ?>
-            <div class="gb-row">
-                <button class="deletebutton"></button>
-                <div class="gb-datetime"><?=$e['datetime']?></div>
-                <div class="gb-name">
-                    <span class="gb-name-a"><?=$e['name']?></span>
-                    <span class="gb-name-b">signed:</span>
+            <form>
+                <div class="gb-row">
+                    <?php if ($e['name'] == $nm) {
+                        echo '<button type="submit" class="deletebutton" name="'.$dels.'"  onclick=""></button>' ?>
+                        <button class="editbutton"></button>
+                    <?php }?>
+                    <div class="gb-datetime"><?=$e['datetime']?></div>
+                    <div class="gb-name">
+                        <span class="gb-name-a"><?=$e['name']?></span>
+                        <span class="gb-name-b">signed:</span>
+                    </div>
+                    <div class="gb-comment"><?=$e['comment']?></div>
                 </div>
-                <div class="gb-comment"><?=$e['comment']?></div>
-            </div>
+            </form>
+
+
         <?php }} ?></div><?php
     $i++;
+    $k++;
 }?>
 <form method="get" target="_self" id="gb-form">
     <input type="submit" value="Go back" name="go"/>
 </form>
 </body>
 </html>
+<?php
+for ($i = 1; $i < $k+1; $i++) {
+    if (isset($_GET['delete'.$i])) {
+        $query1 = "DELETE FROM guestbook WHERE post_id='".$i."'";
+        $result1 = pg_query($db, $query1) or die("Can't find data: " . pg_last_error());
+        $query2 = "UPDATE guestbook SET post_id = post_id - 1 WHERE post_id>'".$i."' ";
+        $result2 = pg_query($db, $query2) or die("Can't find data: " . pg_last_error());
+        break;
+    }
+}
