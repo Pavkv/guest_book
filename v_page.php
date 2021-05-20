77aa1a144aec50d7384e87e2f0dbf66139d8efb5
Pavel <pavel_zobov01@inbox.ru>
2021-05-20 18:22:26 +0300

    yes

--- v_page.php
+++ v_page.php
@@ -1,4 +1,5 @@
 <?php
+require "core.php";
 include('emoji.php');
 $db = pg_connect("host=localhost dbname=guest_book user=pavkv password=8421537690") or die("Can't connect to database".pg_last_error());
 if(isset($_GET['go'])) header('Location: b_page.php');
@@ -14,10 +15,10 @@ if (isset($_GET['delete'])) {
 }
 if(isset($_GET['edited'])){
     $text = $_GET['text'];
+    $i = $_GET['text2'];
     Smilify($text);
-    $i = $_GET['edit'];
-    $query2 = "UPDATE guestbook SET comment = '".strval($text)."' WHERE post_id ='" .$i. "'";
-    $result2 = pg_query($db, $query2) or die("Can't find data: " . pg_last_error());
+    $query3 = "UPDATE guestbook SET comment = '".$text."' WHERE post_id ='".$i."'";
+    $result3 = pg_query($db, $query3) or die("Can't find data: " . pg_last_error());
 }
 ?>
 <!DOCTYPE html>
@@ -28,7 +29,6 @@ if(isset($_GET['edited'])){
 </head>
 <body>
 <?php
-require "core.php";
 $pid = 0;
 $query = "SELECT max(post_id) FROM guestbook";
 $result = pg_query($db, $query) or die("Can't find data: " . pg_last_error());
@@ -60,10 +60,15 @@ while ($pid >= $s){
     $s++;
     $k++;
 }?>
-<form method="get" target="_self" id="gb-form" style="<?php if(!isset($_GET['edit'])) echo 'display:none'?>">
-    <input type="text" name="text" value="<?php  $entries = $_GB->get($_GET['edit']); if (count($entries)>0)
-    { foreach ($entries as $e) { echo $e['comment'];}}?>">
-    <input type="submit" value="save" name="edited">
+<form method="get" target="_self" id="gb-form" style="<?php if(!isset($_GET['edit']) || isset($_GET['canceled'])) echo 'display:none'?>">
+    <input type="hidden" name="text2" value="<?php echo $_GET['edit']; ?>"/><input type="text" name="text" value="<?php  $entries = $_GB->get($_GET['edit']); if (count($entries)>0)
+    { foreach ($entries as $e) { $str = $e['comment']; if(stristr($str, '<')){$strn = explode('<', $str);
+        $strnn = explode('>', $strn[1]);
+        $strnnn = explode(' ', $strnn[0]);
+        foreach ($strnnn as $s){if(stristr($s, 'alt')){$sn = explode('"', $s); $i = $strn[0] . $sn[1]; echo $i;}}}
+    else echo $str;}}?>">
+    <input type="submit" value="Save" name="edited">
+    <input type="submit" value="Cancel" name="canceled">
 </form>
 <form method="get" target="_self" id="gb-form">
     <input type="submit" value="Go back" name="go"/>
